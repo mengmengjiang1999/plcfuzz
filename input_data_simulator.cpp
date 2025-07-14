b475a1363b00f979e2c58c8982f1ab0c9d0ba5e4
萌萌酱 <chenzhangmeng1999@gmail.com>
2025-07-14 23:21:08 +0800

    fix >> bug

--- input_data_simulator.cpp
+++ input_data_simulator.cpp
@@ -36,13 +36,13 @@ std::istream& operator>>(std::istream& is,BoolBlock& sim){
             sim.bool_input[i/8][i%8]=0;
         printf("%d \n",sim.bool_input[i/8][i%8]);
     }
-    //     // 检查行尾是否有额外数据
-    // if (is.peek() == '\n') {
-    //     is.ignore();  // 忽略换行符
-    // } else if (!is.eof()) {
-    //     std::cerr << "警告：行尾有多余数据\n";
-    //     is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    // }
+        // 检查行尾是否有额外数据
+    if (is.peek() == '\n') {
+        is.ignore();  // 忽略换行符
+    } else if (!is.eof()) {
+        std::cerr << "警告：行尾有多余数据\n";
+        is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
     return is;
 }
 
