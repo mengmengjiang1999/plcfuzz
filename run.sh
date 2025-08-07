31c54ce5623ac321361bea158ee03543aa21c9f7
萌萌酱 <chenzhangmeng1999@gmail.com>
2025-08-07 21:53:02 +0800

    调整代码目录

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ g++ -std=gnu++11 -I ./lib -c ./plclogic/Res0.c  -o ./build/Res0.o  -lasiodnp3 -l
 echo "Generating glueVars..."
 ./tools/glue_generator ./plclogic/LOCATED_VARIABLES.h ./src/glueVars.cpp
 echo "Compiling main program..."
-g++ -std=gnu++11  ./src/*.cpp ./build/*.o -o openplc -I ./lib -I ./plclogic/ -pthread -fpermissive `pkg-config --cflags --libs libmodbus` -lasiodnp3 -lasiopal -lopendnp3 -lopenpal -w $ETHERCAT_INC
+g++ -std=gnu++11  ./src/*.cpp ./build/*.o -o openplc -I ./lib -I ./plclogic/ -I ./include/ -pthread -fpermissive `pkg-config --cflags --libs libmodbus` -lasiodnp3 -lasiopal -lopendnp3 -lopenpal -w $ETHERCAT_INC
 echo "Compilation finished successfully!"
 
 ./openplc < input.txt  > output.txt
