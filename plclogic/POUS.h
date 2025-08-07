4a128a7c8e7471289500a83ba0df76e3c480c8ad
萌萌酱 <chenzhangmeng1999@gmail.com>
2025-08-07 21:29:53 +0800

    修改代码目录结构

---
+++ plclogic/POUS.h
@@ -0,0 +1,28 @@
+#ifndef __POUS_H
+#define __POUS_H
+
+#include "accessor.h"
+#include "iec_std_lib.h"
+
+__DECLARE_ENUMERATED_TYPE(LOGLEVEL,
+  LOGLEVEL__CRITICAL,
+  LOGLEVEL__WARNING,
+  LOGLEVEL__INFO,
+  LOGLEVEL__DEBUG
+)
+// PROGRAM PROGRAM0
+// Data part
+typedef struct {
+  // PROGRAM Interface - IN, OUT, IN_OUT variables
+
+  // PROGRAM private variables - TEMP, private and located variables
+  __DECLARE_LOCATED(BOOL,PB1)
+  __DECLARE_LOCATED(BOOL,LED2)
+  __DECLARE_LOCATED(BOOL,LED)
+
+} PROGRAM0;
+
+void PROGRAM0_init__(PROGRAM0 *data__, BOOL retain);
+// Code part
+void PROGRAM0_body__(PROGRAM0 *data__);
+#endif //__POUS_H
--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 CC=gcc CXX=g++
-g++ -std=gnu++11 -I ./lib -c ./buildfiles/Config0.c -lasiodnp3 -lasiopal -lopendnp3 -lopenpal -w 
-g++ -std=gnu++11 -I ./lib -c ./buildfiles/Res0.c -lasiodnp3 -lasiopal -lopendnp3 -lopenpal -w $ETHERCAT_INC
+rm -rf ./build
+mkdir ./build
+g++ -std=gnu++11 -I ./lib -c ./plclogic/Config0.c  -o ./build/Config0.o -lasiodnp3 -lasiopal -lopendnp3 -lopenpal -w 
+g++ -std=gnu++11 -I ./lib -c ./plclogic/Res0.c  -o ./build/Res0.o  -lasiodnp3 -lasiopal -lopendnp3 -lopenpal -w $ETHERCAT_INC
 echo "Generating glueVars..."
-./tools/glue_generator ./buildfiles/LOCATED_VARIABLES.h ./src/glueVars.cpp
+./tools/glue_generator ./plclogic/LOCATED_VARIABLES.h ./src/glueVars.cpp
 echo "Compiling main program..."
-g++ -std=gnu++11  ./src/*.cpp *.o -o openplc -I ./lib -I ./buildfiles/ -pthread -fpermissive `pkg-config --cflags --libs libmodbus` -lasiodnp3 -lasiopal -lopendnp3 -lopenpal -w $ETHERCAT_INC
+g++ -std=gnu++11  ./src/*.cpp ./build/*.o -o openplc -I ./lib -I ./plclogic/ -pthread -fpermissive `pkg-config --cflags --libs libmodbus` -lasiodnp3 -lasiopal -lopendnp3 -lopenpal -w $ETHERCAT_INC
 echo "Compilation finished successfully!"
 
 ./openplc < input.txt  > output.txt
