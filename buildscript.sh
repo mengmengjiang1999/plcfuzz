4025e3d25fd4e3721dcd5582b2e6f55c81f9d08c
萌萌酱 <chenzhangmeng1999@gmail.com>
2025-10-04 15:24:23 +0800

    调整脚本

---
+++ buildscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ./build_scripts/build_plcfiles.sh ./testcases/state_test.st
+# ./build_scripts/build.sh
+# python3 ./static_analyse/main.py
+# ./build_scripts/build_shared_library.sh
+./build_scripts/buildfuzz.sh
