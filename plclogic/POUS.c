22b442c573770d85974ccc30f0f93369a9af3a55
萌萌酱 <chenzhangmeng1999@gmail.com>
2025-10-05 00:47:17 +0800

    add auto

--- plclogic/POUS.c
+++ plclogic/POUS.c
@@ -1,8 +1,12 @@
 void PROGRAM0_init__(PROGRAM0 *data__, BOOL retain) {
   __INIT_VAR(data__->PB1,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->PB2,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->PB3,__BOOL_LITERAL(FALSE),retain)
   __INIT_VAR(data__->LED,__BOOL_LITERAL(FALSE),retain)
   __INIT_VAR(data__->LED2,__BOOL_LITERAL(FALSE),retain)
-  __INIT_VAR(data__->CSTATE92,0,retain)
+  __INIT_VAR(data__->LED3,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->LED4,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->CSTATE44,0,retain)
 }
 
 // Code part
@@ -10,48 +14,62 @@ void PROGRAM0_body__(PROGRAM0 *data__) {
   // Initialise TEMP variables
 
   {
-    LINT __case_expression = __GET_VAR(data__->CSTATE92,);
+    LINT __case_expression = __GET_VAR(data__->CSTATE44,);
     if ((__case_expression == 0)) {
       if (__BOOL_LITERAL(TRUE)) {
-        __SET_VAR(data__->,CSTATE92,,1);
+        __SET_VAR(data__->,CSTATE44,,1);
         __SET_VAR(data__->,LED,,__BOOL_LITERAL(FALSE));
         __SET_VAR(data__->,LED2,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED3,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED4,,__BOOL_LITERAL(FALSE));
       };
     }
     else if ((__case_expression == 1)) {
-      if ((__GET_VAR(data__->PB1,) == __BOOL_LITERAL(FALSE))) {
-        __SET_VAR(data__->,CSTATE92,,1);
+      if ((((__GET_VAR(data__->PB1,) == __BOOL_LITERAL(FALSE)) && __BOOL_LITERAL(TRUE)) && __BOOL_LITERAL(TRUE))) {
+        __SET_VAR(data__->,CSTATE44,,1);
         __SET_VAR(data__->,LED,,__BOOL_LITERAL(TRUE));
         __SET_VAR(data__->,LED2,,__BOOL_LITERAL(FALSE));
-      } else if ((__GET_VAR(data__->PB1,) == __BOOL_LITERAL(TRUE))) {
-        __SET_VAR(data__->,CSTATE92,,2);
+        __SET_VAR(data__->,LED3,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED4,,__BOOL_LITERAL(FALSE));
+      } else if ((((__GET_VAR(data__->PB1,) == __BOOL_LITERAL(TRUE)) && __BOOL_LITERAL(TRUE)) && __BOOL_LITERAL(TRUE))) {
+        __SET_VAR(data__->,CSTATE44,,3);
         __SET_VAR(data__->,LED,,__BOOL_LITERAL(TRUE));
         __SET_VAR(data__->,LED2,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED3,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED4,,__BOOL_LITERAL(FALSE));
       };
     }
     else if ((__case_expression == 2)) {
-      if ((__GET_VAR(data__->PB1,) == __BOOL_LITERAL(FALSE))) {
-        __SET_VAR(data__->,CSTATE92,,3);
-        __SET_VAR(data__->,LED,,__BOOL_LITERAL(TRUE));
-        __SET_VAR(data__->,LED2,,__BOOL_LITERAL(TRUE));
-      } else if ((__GET_VAR(data__->PB1,) == __BOOL_LITERAL(TRUE))) {
-        __SET_VAR(data__->,CSTATE92,,4);
-        __SET_VAR(data__->,LED,,__BOOL_LITERAL(TRUE));
-        __SET_VAR(data__->,LED2,,__BOOL_LITERAL(TRUE));
+      if (__BOOL_LITERAL(TRUE)) {
+        __SET_VAR(data__->,CSTATE44,,1);
+        __SET_VAR(data__->,LED,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED2,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED3,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED4,,__BOOL_LITERAL(FALSE));
       };
     }
     else if ((__case_expression == 3)) {
-      if (__BOOL_LITERAL(TRUE)) {
-        __SET_VAR(data__->,CSTATE92,,1);
-        __SET_VAR(data__->,LED,,__BOOL_LITERAL(FALSE));
-        __SET_VAR(data__->,LED2,,__BOOL_LITERAL(FALSE));
+      if ((((__GET_VAR(data__->PB1,) == __BOOL_LITERAL(FALSE)) && __BOOL_LITERAL(TRUE)) && __BOOL_LITERAL(TRUE))) {
+        __SET_VAR(data__->,CSTATE44,,2);
+        __SET_VAR(data__->,LED,,__BOOL_LITERAL(TRUE));
+        __SET_VAR(data__->,LED2,,__BOOL_LITERAL(TRUE));
+        __SET_VAR(data__->,LED3,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED4,,__BOOL_LITERAL(FALSE));
+      } else if ((((__GET_VAR(data__->PB1,) == __BOOL_LITERAL(TRUE)) && __BOOL_LITERAL(TRUE)) && __BOOL_LITERAL(TRUE))) {
+        __SET_VAR(data__->,CSTATE44,,4);
+        __SET_VAR(data__->,LED,,__BOOL_LITERAL(TRUE));
+        __SET_VAR(data__->,LED2,,__BOOL_LITERAL(TRUE));
+        __SET_VAR(data__->,LED3,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED4,,__BOOL_LITERAL(FALSE));
       };
     }
     else if ((__case_expression == 4)) {
       if (__BOOL_LITERAL(TRUE)) {
-        __SET_VAR(data__->,CSTATE92,,3);
+        __SET_VAR(data__->,CSTATE44,,2);
         __SET_VAR(data__->,LED,,__BOOL_LITERAL(FALSE));
         __SET_VAR(data__->,LED2,,__BOOL_LITERAL(TRUE));
+        __SET_VAR(data__->,LED3,,__BOOL_LITERAL(FALSE));
+        __SET_VAR(data__->,LED4,,__BOOL_LITERAL(FALSE));
       };
     }
   };
